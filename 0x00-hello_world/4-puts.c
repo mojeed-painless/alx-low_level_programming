d637ba72e38bd15b814728f5feda208870a03f95
mojeed-painless <shittuola47@gmail.com>
2023-10-05 17:46:48 -0700

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+*main - function
+*return - 0 means success
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
