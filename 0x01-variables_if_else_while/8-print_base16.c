9609c2c64980b663adb56550a75f5cdfa9a619d8
mojeed-painless <shittuola47@gmail.com>
2023-10-06 20:56:48 -0700

    updated

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: 'Print base 16 numbers in lowercase'
+ *Return: always 0
+ */
+int main(void)
+{
+	int k;
+	int j = 97;
+
+	for (k = 48; k <= 97; k++)
+	{
+		putchar(k);
+	}
+	while (j <= 102)
+	{
+		putchar(j);
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
