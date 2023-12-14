efc6f885eb99602133fa79a461e892aad6be1a14
mojeed-painless <shittuola47@gmail.com>
2023-12-13 16:39:49 -0800

    updated

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits.
+ * @n: number one.
+ * @m: number two.
+ * Return: number of bits.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int n_bits;
+
+	for (n_bits = 0; n || m; n >>= 1, m >>= 1)
+	{
+		if ((n & 1) != (m & 1))
+			n_bits++;
+	}
+
+	return (n_bits);
+}
